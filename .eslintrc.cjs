fd84e60d4326f50d169aa214cd0fe6635428da1f
Bohdan Leonets <leonetsbogdan@gmail.com>
2023-07-06 18:51:56 +0300

    fix: @indielayer/eslint-config-vue was removed, some changes to vite config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
-    '@indielayer',
     'eslint:recommended',
     'plugin:vue/recommended',
   ],
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,19 @@
-import * as path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'; 
-import eslint from 'vite-plugin-eslint';
-
-function resolve(dir) {
-  return path.resolve(__dirname, dir);
-}
 
 export default defineConfig({
   base: '/',
   plugins: [
     vue(),
-    eslint({
-      fix: true,
-    }),
   ],
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      '@': resolve('./src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
